eb10dfb903bfe91675fa11fca90660cc757527e1
Larry Chiem <clarry@pdx.edu>
2021-05-30 17:15:23 -0700

    variables strings and numbers

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nameOne string = "mario"
+	// var nameTwo string = "luigi"
+	// var nameThree string
+	// fmt.Println(nameOne + " and " + nameTwo, nameThree)
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameOne = "Peach"
+	// nameThree = "Bowser"
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "yoshi" 
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits and memory
+}
